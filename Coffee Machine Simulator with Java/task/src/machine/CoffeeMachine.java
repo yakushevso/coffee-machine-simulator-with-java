9d2df58946aa8450ca0a80990d6836dccd5daf1a
Work <yakushevso@ya.ru>
2024-03-27 08:29:33 +0300

    Stage 2/6: Calculate the ingredients

--- Coffee Machine Simulator with Java/task/src/machine/CoffeeMachine.java
+++ Coffee Machine Simulator with Java/task/src/machine/CoffeeMachine.java
@@ -1,14 +1,22 @@
 package machine;
 
+import java.util.Scanner;
+
 public class CoffeeMachine {
     public static void main(String[] args) {
-        System.out.println("""
-                Starting to make a coffee
-                Grinding coffee beans
-                Boiling water
-                Mixing boiled water with crushed coffee beans
-                Pouring coffee into the cup
-                Pouring some milk into the cup
-                Coffee is ready!""");
+        try (Scanner sc = new Scanner(System.in)) {
+            int cups = sc.nextInt();
+            System.out.printf("For %d cups of coffee you will need:\n", cups);
+            calculator(cups);
+        }
+    }
+
+    public static void calculator(int cups) {
+        int water = 200;
+        int milk = 50;
+        int coffee = 15;
+
+        System.out.printf("%d ml of water\n%d ml of milk\n%d g of coffee beans",
+                water * cups, milk * cups, coffee * cups);
     }
 }
